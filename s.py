f73b900fbf5e232af50268a667a56260b5915cf3
acz <sompolygosundiszno@gmail.com>
2026-05-05 16:26:02 +0200

    added instruction text and exception handling

--- s.py
+++ s.py
@@ -5,43 +5,46 @@ from pygame import mixer
 pygame.init()
 pygame.mixer.set_num_channels(50)
 
-font = pygame.font.Font('assets/Terserah.ttf', 48)
-medium_font = pygame.font.Font('assets/Terserah.ttf', 28)
-small_font = pygame.font.Font('assets/Terserah.ttf', 16)
-real_small_font = pygame.font.Font('assets/Terserah.ttf', 10)
-
-timer=pygame.time.Clock()
+font = pygame.font.Font('assets/AbhayaLibre-Regular.ttf', 48)
+medium_font = pygame.font.Font('assets/AbhayaLibre-Regular.ttf', 28)
+small_font = pygame.font.Font('assets/AbhayaLibre-Regular.ttf', 16)
+real_small_font = pygame.font.Font('assets/AbhayaLibre-Regular.ttf', 10)
 fps = 60
-WIDTH=52 * 35
-HEIGHT=400
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("PYano")
-
-active_whites = []
-active_blacks = []
+timer = pygame.time.Clock()
+WIDTH = 52 * 35
+HEIGHT = 400
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
 white_sounds = []
 black_sounds = []
-
+active_whites = []
+active_blacks = []
 left_oct = 4
 right_oct = 5
 
+left_hand = pl.left_hand
+right_hand = pl.right_hand
+piano_notes = pl.piano_notes
+white_notes = pl.white_notes
+black_notes = pl.black_notes
+black_labels = pl.black_labels
 
-for i in range(len(pl.white_notes)):
-    white_sounds.append(mixer.Sound(f'assets\\notes\\{pl.white_notes[i]}.wav'))
-    
-for i in range(len(pl.black_notes)):
-    black_sounds.append(mixer.Sound(f'assets\\notes\\{pl.black_notes[i]}.wav'))
+for i in range(len(white_notes)):
+    white_sounds.append(mixer.Sound(f'assets\\notes\\{white_notes[i]}.wav'))
 
+for i in range(len(black_notes)):
+    black_sounds.append(mixer.Sound(f'assets\\notes\\{black_notes[i]}.wav'))
+
+pygame.display.set_caption("Pete's Python Piano")
 
 
 def draw_piano(whites, blacks):
-    white_rects=[]
+    white_rects = []
     for i in range(52):
-        rect = pygame.draw.rect(screen, "white", [i*35, HEIGHT-300, 35,300],0,2)
+        rect = pygame.draw.rect(screen, 'white', [i * 35, HEIGHT - 300, 35, 300], 0, 2)
         white_rects.append(rect)
-        pygame.draw.rect(screen, "black", [i*35, HEIGHT-300, 35,300],2,2)
-        key_label = small_font.render(pl.white_notes[i], True, "black")
-        screen.blit(key_label, (i*35+3, HEIGHT-20))
+        pygame.draw.rect(screen, 'black', [i * 35, HEIGHT - 300, 35, 300], 2, 2)
+        key_label = small_font.render(white_notes[i], True, 'black')
+        screen.blit(key_label, (i * 35 + 3, HEIGHT - 20))
     skip_count = 0
     last_skip = 2
     skip_track = 2
@@ -53,7 +56,8 @@ def draw_piano(whites, blacks):
                 if blacks[q][1] > 0:
                     pygame.draw.rect(screen, 'green', [23 + (i * 35) + (skip_count * 35), HEIGHT - 300, 24, 200], 2, 2)
                     blacks[q][1] -= 1
-        key_label = small_font.render(pl.black_labels[i], True, "white")
+
+        key_label = real_small_font.render(black_labels[i], True, 'white')
         screen.blit(key_label, (25 + (i * 35) + (skip_count * 35), HEIGHT - 120))
         black_rects.append(rect)
         skip_track += 1
@@ -66,16 +70,85 @@ def draw_piano(whites, blacks):
             skip_track = 0
             skip_count += 1
 
-        for i in range(len(whites)):
-             if whites[i][1] > 0:
-                j = whites[i][0]
-                pygame.draw.rect(screen, 'green', [j * 35, HEIGHT - 100, 35, 100], 2, 2)
-                whites[i][1] -= 1
+    for i in range(len(whites)):
+        if whites[i][1] > 0:
+            j = whites[i][0]
+            pygame.draw.rect(screen, 'green', [j * 35, HEIGHT - 100, 35, 100], 2, 2)
+            whites[i][1] -= 1
 
     return white_rects, black_rects, whites, blacks
 
-running = True
-while running:
+
+def draw_hands(rightOct, leftOct, rightHand, leftHand):
+    # left hand
+    pygame.draw.rect(screen, 'dark gray', [(leftOct * 245) - 175, HEIGHT - 60, 245, 30], 0, 4)
+    pygame.draw.rect(screen, 'black', [(leftOct * 245) - 175, HEIGHT - 60, 245, 30], 4, 4)
+    text = small_font.render(leftHand[0], True, 'white')
+    screen.blit(text, ((leftOct * 245) - 165, HEIGHT - 55))
+    text = small_font.render(leftHand[2], True, 'white')
+    screen.blit(text, ((leftOct * 245) - 130, HEIGHT - 55))
+    text = small_font.render(leftHand[4], True, 'white')
+    screen.blit(text, ((leftOct * 245) - 95, HEIGHT - 55))
+    text = small_font.render(leftHand[5], True, 'white')
+    screen.blit(text, ((leftOct * 245) - 60, HEIGHT - 55))
+    text = small_font.render(leftHand[7], True, 'white')
+    screen.blit(text, ((leftOct * 245) - 25, HEIGHT - 55))
+    text = small_font.render(leftHand[9], True, 'white')
+    screen.blit(text, ((leftOct * 245) + 10, HEIGHT - 55))
+    text = small_font.render(leftHand[11], True, 'white')
+    screen.blit(text, ((leftOct * 245) + 45, HEIGHT - 55))
+    text = small_font.render(leftHand[1], True, 'black')
+    screen.blit(text, ((leftOct * 245) - 148, HEIGHT - 55))
+    text = small_font.render(leftHand[3], True, 'black')
+    screen.blit(text, ((leftOct * 245) - 113, HEIGHT - 55))
+    text = small_font.render(leftHand[6], True, 'black')
+    screen.blit(text, ((leftOct * 245) - 43, HEIGHT - 55))
+    text = small_font.render(leftHand[8], True, 'black')
+    screen.blit(text, ((leftOct * 245) - 8, HEIGHT - 55))
+    text = small_font.render(leftHand[10], True, 'black')
+    screen.blit(text, ((leftOct * 245) + 27, HEIGHT - 55))
+    # right hand
+    pygame.draw.rect(screen, 'dark gray', [(rightOct * 245) - 175, HEIGHT - 60, 245, 30], 0, 4)
+    pygame.draw.rect(screen, 'black', [(rightOct * 245) - 175, HEIGHT - 60, 245, 30], 4, 4)
+    text = small_font.render(rightHand[0], True, 'white')
+    screen.blit(text, ((rightOct * 245) - 165, HEIGHT - 55))
+    text = small_font.render(rightHand[2], True, 'white')
+    screen.blit(text, ((rightOct * 245) - 130, HEIGHT - 55))
+    text = small_font.render(rightHand[4], True, 'white')
+    screen.blit(text, ((rightOct * 245) - 95, HEIGHT - 55))
+    text = small_font.render(rightHand[5], True, 'white')
+    screen.blit(text, ((rightOct * 245) - 60, HEIGHT - 55))
+    text = small_font.render(rightHand[7], True, 'white')
+    screen.blit(text, ((rightOct * 245) - 25, HEIGHT - 55))
+    text = small_font.render(rightHand[9], True, 'white')
+    screen.blit(text, ((rightOct * 245) + 10, HEIGHT - 55))
+    text = small_font.render(rightHand[11], True, 'white')
+    screen.blit(text, ((rightOct * 245) + 45, HEIGHT - 55))
+    text = small_font.render(rightHand[1], True, 'black')
+    screen.blit(text, ((rightOct * 245) - 148, HEIGHT - 55))
+    text = small_font.render(rightHand[3], True, 'black')
+    screen.blit(text, ((rightOct * 245) - 113, HEIGHT - 55))
+    text = small_font.render(rightHand[6], True, 'black')
+    screen.blit(text, ((rightOct * 245) - 43, HEIGHT - 55))
+    text = small_font.render(rightHand[8], True, 'black')
+    screen.blit(text, ((rightOct * 245) - 8, HEIGHT - 55))
+    text = small_font.render(rightHand[10], True, 'black')
+    screen.blit(text, ((rightOct * 245) + 27, HEIGHT - 55))
+
+
+def draw_title_bar():
+    instruction_text = medium_font.render('Fel/Le nyilakkal halad a bal kéz', True, 'black')
+    screen.blit(instruction_text, (WIDTH - 500, 10))
+    instruction_text2 = medium_font.render('Bal/Jobb nyilakkal halad a jobb kéz', True, 'black')
+    screen.blit(instruction_text2, (WIDTH - 500, 50))
+    title_text = font.render('Python zongora!', True, 'white')
+    screen.blit(title_text, (250, 18))
+    title_text = font.render('Python zongora!', True, 'black')
+    screen.blit(title_text, (252, 20))
+
+
+run = True
+while run:
     left_dict = {'Z': f'C{left_oct}',
                  'S': f'C#{left_oct}',
                  'X': f'D{left_oct}',
@@ -102,45 +175,60 @@ while running:
                   '0': f'A#{right_oct}',
                   'P': f'B{right_oct}'}
     timer.tick(fps)
-    screen.fill("grey")
+    screen.fill('gray')
     white_keys, black_keys, active_whites, active_blacks = draw_piano(active_whites, active_blacks)
-
-
+    draw_hands(right_oct, left_oct, right_hand, left_hand)
+    draw_title_bar()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-            
+            run = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             black_key = False
             for i in range(len(black_keys)):
                 if black_keys[i].collidepoint(event.pos):
-                    black_sounds[i].play(0,1000)
+                    black_sounds[i].play(0, 1000)
                     black_key = True
-                    active_blacks.append([i,30])
+                    active_blacks.append([i, 30])
             for i in range(len(white_keys)):
                 if white_keys[i].collidepoint(event.pos) and not black_key:
-                    white_sounds[i].play(0,1000)
-                    active_whites.append([i,30])
+                    white_sounds[i].play(0, 3000)
+                    active_whites.append([i, 30])
         if event.type == pygame.TEXTINPUT:
-            if event.text.upper() in left_dict:
-                if left_dict[event.text.upper()][1] == '#':
-                    index = pl.black_labels.index(left_dict[event.text.upper()])
-                    black_sounds[index].play(0, 1000)
-                    active_blacks.append([index, 30])
-                else:
-                    index = pl.white_notes.index(left_dict[event.text.upper()])
-                    white_sounds[index].play(0, 1000)
-                    active_whites.append([index, 30])
-            if event.text.upper() in right_dict:
-                if right_dict[event.text.upper()][1] == '#':
-                    index = pl.black_labels.index(right_dict[event.text.upper()])
-                    black_sounds[index].play(0, 1000)
-                    active_blacks.append([index, 30])
-                else:
-                    index = pl.white_notes.index(right_dict[event.text.upper()])
-                    white_sounds[index].play(0, 1000)
-                    active_whites.append([index, 30])
+            try:
+                if event.text.upper() in left_dict:
+                    if left_dict[event.text.upper()][1] == '#':
+                        index = black_labels.index(left_dict[event.text.upper()])
+                        black_sounds[index].play(0, 1000)
+                        active_blacks.append([index, 30])
+                    else:
+                        index = white_notes.index(left_dict[event.text.upper()])
+                        white_sounds[index].play(0, 1000)
+                        active_whites.append([index, 30])
+                if event.text.upper() in right_dict:
+                    if right_dict[event.text.upper()][1] == '#':
+                        index = black_labels.index(right_dict[event.text.upper()])
+                        black_sounds[index].play(0, 1000)
+                        active_blacks.append([index, 30])
+                    else:
+                        index = white_notes.index(right_dict[event.text.upper()])
+                        white_sounds[index].play(0, 1000)
+                        active_whites.append([index, 30])
+            except ValueError:
+                pass
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                if right_oct < 8:
+                    right_oct += 1
+            if event.key == pygame.K_LEFT:
+                if right_oct > 0:
+                    right_oct -= 1
+            if event.key == pygame.K_UP:
+                if left_oct < 8:
+                    left_oct += 1
+            if event.key == pygame.K_DOWN:
+                if left_oct > 0:
+                    left_oct -= 1
 
     pygame.display.flip()
-
 pygame.quit()
